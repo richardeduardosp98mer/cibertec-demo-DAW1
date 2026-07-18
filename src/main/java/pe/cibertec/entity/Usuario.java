444e8456321b39120e23029ed38fdd567ceedbf9
= <=>
2026-07-18 15:39:09 -0500

    Inicio Clase 3

--- src/main/java/pe/cibertec/entity/Usuario.java
+++ src/main/java/pe/cibertec/entity/Usuario.java
@@ -10,10 +10,10 @@ public class Usuario {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
-    @Column(nullable = false)
+    @Column(nullable = false, length = 100)
     private String nombre;
-    @Column(nullable = false)
+    @Column(nullable = false, length = 100)
     private String correo;
-    @Column(nullable = false)
+    @Column(nullable = false, length = 8)
     private String clave;
 }
