d051f41e82cd519d7206643be76cb60fd0ebe966
= <=>
2026-07-18 16:36:20 -0500

    Producto Controller

---
+++ src/main/java/pe/cibertec/Controller/ProductoController.java
@@ -0,0 +1,26 @@
+package pe.cibertec.Controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import pe.cibertec.entity.Producto;
+import pe.cibertec.servicie.ProductoService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/productos")
+public class ProductoController {
+
+    private final ProductoService productoService;
+
+    public ProductoController(ProductoService productoService) {
+        this.productoService = productoService;
+    }
+
+    public ResponseEntity<String> registrarLote(@RequestBody List<Producto> productos){
+        productoService.registrarLote((productos));
+        return ResponseEntity.ok("Productos Registrados satisfactoriamente");
+    }
+}
--- src/main/java/pe/cibertec/servicie/ProductoService.java
+++ src/main/java/pe/cibertec/servicie/ProductoService.java
@@ -4,7 +4,10 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import org.springframework.stereotype.Service;
+import pe.cibertec.entity.Producto;
 import pe.cibertec.repository.ProductoRepository;
+
+import java.util.List;
 
 @Service
 public class ProductoService {
@@ -15,5 +18,18 @@ public class ProductoService {
 
     public ProductoService(ProductoRepository productoRepository) {
         this.productoRepository = productoRepository;
+    }
+
+    // Insercion por lotes (batch insert)
+    public void registrarLote(List<Producto> productos){
+        int i =0;
+        for (Producto p: productos){
+            em.persist(p);
+            i++;
+            if (i % 10 ==0){  //arma el paginado
+                em.flush();
+                em.clear();
+            }
+        }
     }
 }
