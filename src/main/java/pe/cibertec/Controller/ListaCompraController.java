e0ab36b70a51420758a28fa18139c439ef2908ab
= <=>
2026-07-25 17:57:22 -0500

    HistorialCompra

--- src/main/java/pe/cibertec/Controller/ListaCompraController.java
+++ src/main/java/pe/cibertec/Controller/ListaCompraController.java
@@ -2,11 +2,14 @@ package pe.cibertec.Controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import pe.cibertec.entity.ItemLista;
 import pe.cibertec.entity.ListaCompra;
 import pe.cibertec.entity.Usuario;
 import pe.cibertec.repository.ItemRepository;
 import pe.cibertec.repository.ListaRepository;
 import pe.cibertec.repository.UsuarioRepository;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("api/listas")
@@ -29,5 +32,28 @@ public class ListaCompraController {
         }
         listaCompra.setUsuario(usuario);
         return  ResponseEntity.ok(listaRepository.save(listaCompra));
+    }
+
+    @PostMapping("{idLista}/agregar-ietm")
+    public ResponseEntity<?> agregarLista(@PathVariable Long idLista, @RequestBody ItemLista itemLista){
+        ListaCompra listaCompra = listaRepository.findById(idLista).orElse(null);
+        if (itemLista == null){
+            return ResponseEntity.notFound().build();
+        }
+        itemLista.setLista(listaCompra);
+        return ResponseEntity.ok(itemRepository.save(itemLista));
+    }
+
+    @PostMapping("ietm/{idItem}/estado")
+    public ResponseEntity<?> cambiarEstado(@PathVariable Long idItem, @RequestParam String estado){
+        return itemRepository.findById(idItem).map(item -> {
+            item.setEstado(estado);
+            return ResponseEntity.ok(itemRepository.save(item));
+        }).orElse(ResponseEntity.notFound().build());
+    }
+
+    @GetMapping("usuario/{idUsuario}")
+    public List<ListaCompra> historial(@PathVariable Long idUsuario){
+        return ListaRepository.findByUsuarioId(idUsuario);
     }
 }
